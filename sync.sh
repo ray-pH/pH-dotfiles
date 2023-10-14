8844b00bc2c9576e4d2a5ed30a036163d3d9d05f
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-10-14 22:15:32 +0700

    add sync.sh

---
+++ sync.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -xe
+cp ~/.bash_aliases .
+cp ~/.bash_prompt .
+cp ~/.bashrc .
+cp ~/.ghc/ghci.conf ./.ghc/ghci.conf
+cp ~/.profile .
+cp ~/.pystart .
+cp ~/.Xresources .
+cp ~/.xsession .
+cp ~/.zsh_prompt .
+cp ~/.zshrc .
+
+# config
+cp ~/.config/alacritty/alacritty.yml ./.config/alacritty/alacritty.yml
+cp ~/.config/i3/config ./.config/i3/config
+cp ~/.config/joshuto/* ./.config/joshuto/
+cp ~/.config/picom.conf ./.config/picom.conf
+cp ~/.config/polybar/* ./.config/polybar/ -r
